a21fbee05d3a03c7eaca64d6b976dad8cf44b5f2
J. A. Whye <outlawgametools@gmail.com>
2013-04-18 04:18:27 -0800

    Just testing making an edit. Because git is the voodoo. :(

--- lime/lime-tile.lua
+++ lime/lime-tile.lua
@@ -15,7 +15,7 @@
 ----									REQUIRED MODULES										----
 ----------------------------------------------------------------------------------------------------
 
-local sprite = require("sprite")
+local sprite = require("sprite") -- doh! old sprite stuff needs updated!
 
 ----------------------------------------------------------------------------------------------------
 ----									CLASS METATABLE											----
